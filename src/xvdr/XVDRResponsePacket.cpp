bbcf3971b9b05783cbd0aed2f5238a7ef9c72be2
Alexander Pipelka <alexander.pipelka@gmail.com>
2012-01-04 22:04:49 +0100

    sanity check before trying to uncompress a packet

--- src/xvdr/XVDRResponsePacket.cpp
+++ src/xvdr/XVDRResponsePacket.cpp
@@ -184,13 +184,21 @@ uint8_t* cXVDRResponsePacket::getUserData()
 bool cXVDRResponsePacket::uncompress()
 {
 #ifdef HAVE_ZLIB
+  if(userDataLength < 6) {
+    XBMC->Log(LOG_DEBUG, "%s - invalid packet size", __FUNCTION__);
+    return false;
+  }
+
   XBMC->Log(LOG_DEBUG, "Uncompressing packet (%i bytes) ...", userDataLength - 4);
 
   uLongf original_size = ntohl(*(uint32_t*)&userData[0]);
   uint8_t* buffer = (uint8_t*)malloc(original_size);
 
   if(buffer == NULL)
+  {
+    userDataLength = 0;
     return false;
+  }
 
   if(::uncompress(buffer, &original_size, userData + 4, userDataLength - 4) == Z_OK)
   {
